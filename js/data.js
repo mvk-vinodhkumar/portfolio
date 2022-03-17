463fb4791f7b3fe72f59d3807d695247b65b64a9
vinodhkumarvk <mvk.vinodhkumar@gmail.com>
2022-03-17 17:35:05 +0530

    Updates on My Works data

--- js/data.js
+++ js/data.js
@@ -180,44 +180,50 @@ function populateWebApps() {
       name: "SYNC",
       desc: "With SYNC, you will be able to share organise and share any type of file within your company and share the same with employees. Customizing of access levels of individual files will ensure your control over your data and seamless sharing improves communication and efficiency.",
       roles: ["Wireframe", "Design", "Front-end"],
-      link: "https://www.builtech.co.in/",
+      link: "",
     },
     {
       name: "Asset Management",
       desc: "Our asset management software enables you to store and access all relevant data regarding your company’s asset procurement cycle and related information with a few clicks. The goal is to make recall of such information simple and quick.",
       roles: ["Design", "Front-end"],
-      link: "https://www.builtech.co.in/",
+      link: "",
     },
     {
       name: "Frequency",
       desc: "It acts as a normal Tablet but is equipped with a fingerprint reader that can be used to record login and logout times of employees along with geo-tagging capabilities. This tablet will help strengthen security in and around the campus. It comes with a rugged casing that can withstand drops and shocks and can be connected to a network through Wi-fi or mobile data (3G Sim). It is easy to install without the need for any cable and retains data even in case of power failure. By providing for simple integration with Six30 labs’ other products, this tablet augments data management for your company.",
       roles: ["Front-end"],
-      link: "https://www.builtech.co.in/",
+      link: "",
     },
     {
       name: "Visitor Management System",
       desc: "V.M.S. can be integrated with the ‘Frequency’ Tablet to document and track visitors within the campus. The ‘Frequency’ tablet can be used to record required data from the visitor and automatically generate an E-pass which would essentially constitute their name, type, entry time and the host(employee) name & designation. The system can be fashioned to accept different types of visitors based on requirements and even be used to blacklist certain people and effectively barring them from entry. V.M.S, too is ready for integration with six30 labs’ other products.",
       roles: ["Wireframe", "Design", "Front-end"],
-      link: "https://www.builtech.co.in/",
+      link: "",
     },
     {
       name: "Management Information System",
       desc: "A streamlined approach to storing and modifying employee records, working hours and requests. This portal allows the functionality of providing access to records based on hierarchy and clocking multiple projects simultaneously. In addition, leave & travel requests can be submitted and approved effortlessly.",
       roles: ["Wireframe", "Design", "Front-end", "Technical Coordinator"],
-      link: "https://www.builtech.co.in/",
+      link: "",
     },
     {
       name: "HRMS",
       desc: "A portal catering to all needs in Human Resource management. A database of employee information, leave management, travel authorisations, probation assessment details, appraisal processing and so on.",
       roles: ["Wireframe", "Design", "Front-end"],
-      link: "https://www.builtech.co.in/",
+      link: "",
     },
-    // {
-    //   name: "Client Project Master",
-    //   desc: "This is a Portal to access details and progress reports of client projects. A system of providing different levels of access to admin, company worker and clients with visually appealing and elaborate project information to clients whereas the admin is given basic progress reports of projects.",
-    //   roles: ["Wireframe", "Design", "Front-end"],
-    //   link: "https://www.builtech.co.in/",
-    // },
+    {
+      name: "Client Project Master",
+      desc: "This is a Portal to access details and progress reports of client projects. A system of providing different levels of access to admin, company worker and clients with visually appealing and elaborate project information to clients whereas the admin is given basic progress reports of projects.",
+      roles: ["Wireframe", "Design", "Front-end"],
+      link: "",
+    },
+    {
+      name: "Respect Your Artist",
+      desc: "A web-app that provides a platform for any type of artist to express their ideologies through videos and podcasts.",
+      roles: ["Content Strategy", "Technical Coordinator"],
+      link: "",
+    },
   ];
 
   function rolesList(roles) {
